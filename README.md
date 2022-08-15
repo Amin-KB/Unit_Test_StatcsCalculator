b04a4c42ac51bafa03aafd6ffb7a31c951b281ff
Amin-KB <aminkarambeigi94@gmail.com>
2022-08-15 07:45:25 +0200

    Single mode Methode

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 "# Unit_Test_StatcsCalculator" 
 
 Test und Debuging übung 09/08/2022
+I worked on GetSingleMode() but i have a problem with GetSingleModeExceptionTest2()
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -8,21 +8,21 @@ namespace UnitTest
 {
     public class UnitTest1
     {
-        
+
 
         [Fact]
         public void Test1()
         {
-          
+
 
         }
         [Theory]
-        [InlineData(2,8,5,5)]
-        [InlineData(10,8,6,8)]
-        [InlineData(2,5,5,4)]        
-        [InlineData(-2,-50,-98,-50)]
-        [InlineData(2.5,4.6,5,4)]
-        public void TestingAccuracyOfAverageMethode(int a,int b,int c,decimal expectedResult)
+        [InlineData(2, 8, 5, 5)]
+        [InlineData(10, 8, 6, 8)]
+        [InlineData(2, 5, 5, 4)]
+        [InlineData(-2, -50, -98, -50)]
+        [InlineData(2.5, 4.6, 5, 4)]
+        public void TestingAccuracyOfAverageMethode(int a, int b, int c, decimal expectedResult)
         {
             //Arrange
             StatCalc calc = new StatCalc();
@@ -47,10 +47,10 @@ namespace UnitTest
             Assert.Throws<AverageCalculationException>(() => calc.CalculateAverage());
         }
         [Theory]
-        [InlineData(2, 8, 5,10, 5)]
-        [InlineData(10, 8, 6,25, 8)]
-        [InlineData(2, 5, 5, 59,4)]
-        public void RemoveLastNumberMethodeTest1(int a, int b, int c,int d, decimal expectedResult)
+        [InlineData(2, 8, 5, 10, 5)]
+        [InlineData(10, 8, 6, 25, 8)]
+        [InlineData(2, 5, 5, 59, 4)]
+        public void RemoveLastNumberMethodeTest1(int a, int b, int c, int d, decimal expectedResult)
         {
             //Arrange
             StatCalc calc = new StatCalc();
@@ -58,11 +58,11 @@ namespace UnitTest
             calc.AddNumber(b);
             calc.AddNumber(c);
             calc.AddNumber(d);
-           
+
             //Act
             calc.RemoveLastNumber();
             calc.CalculateAverage();
-            
+
             //Assert
             Assert.Equal(expectedResult, calc.CalculateAverage());
 
@@ -83,8 +83,8 @@ namespace UnitTest
 
 
             //Act
-            
-           int[] arr= calc.GetNumbers();
+
+            int[] arr = calc.GetNumbers();
             calc.CalculateAverage();
 
             //Assert
@@ -95,7 +95,7 @@ namespace UnitTest
         }
 
         [Fact]
-        public void GetSingModeExceptionTest1()
+        public void GetSingleModeExceptionTest1()
         {
             StatCalc calc = new StatCalc();
             calc.AddNumber(5);
@@ -106,6 +106,36 @@ namespace UnitTest
 
             Assert.Throws<InvalidModeCalculationException>(() => calc.GetSingleMode());
         }
-       
+        [Fact]
+        public void GetSingleModeTest1()
+        {
+         int[] arr = new int[10] { 1, 25, 8, 32, 1, 5, 5, 4, 8, 8 };
+        StatCalc calc = new StatCalc(arr);
+           var result =calc.GetSingleMode();
+            Assert.Equal(8, result);
+
+
+        }
+        [Fact]
+        public void GetSingleModeExceptionTest2()
+        {
+            //beim SingleModeNotFoundException gibt ein Problem
+
+            //Assert
+            int[] arr = new int[4] { 1, 1, 8 ,8};
+            
+            StatCalc calc = new StatCalc(arr);
+
+            calc.GetSingleMode();
+            
+            Assert.Throws<SingleModeNotFoundException>(() => calc.GetSingleMode());
+            
+
+
+
+        }
+
+
+
     }
 }
